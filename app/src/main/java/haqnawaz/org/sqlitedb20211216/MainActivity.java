6747b098fdc4ebafe67edb713b839e75ab4dbb0c
Shan Zafar <bitf18545@pucit.edu.pk>
2021-12-27 14:47:05 +0500

    Another Commit

--- app/src/main/java/haqnawaz/org/sqlitedb20211216/MainActivity.java
+++ app/src/main/java/haqnawaz/org/sqlitedb20211216/MainActivity.java
@@ -1,35 +1,41 @@
 package haqnawaz.org.sqlitedb20211216;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.database.sqlite.SQLiteDatabase;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    Button buttonAdd, buttonViewAll;
+    Button buttonAdd, buttonViewAll, btnDelAll;
     EditText editName, editAge;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     Switch switchIsActive;
-    ListView listViewStudent;
+    ArrayList<StudentModel> list;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonAdd = findViewById(R.id.addBtn);
         buttonViewAll = findViewById(R.id.buttonViewAll);
-        editName = findViewById(R.id.editTextName);
-        editAge = findViewById(R.id.editTextAge);
-        switchIsActive = findViewById(R.id.switchStudent);
-        listViewStudent = findViewById(R.id.listViewStudent);
+        btnDelAll = findViewById(R.id.delBtn);
+
+        recyclerView = findViewById(R.id.myRecyclerView);
+        layoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
 
         buttonAdd.setOnClickListener(new View.OnClickListener() {
             StudentModel studentModel;
