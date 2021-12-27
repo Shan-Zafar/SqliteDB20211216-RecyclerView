0b4398c3640244b8a560dc2fe4af0e9ad3273462
Shan Zafar <bitf18545@pucit.edu.pk>
2021-12-27 15:11:24 +0500

    Another Commit

--- app/src/main/java/haqnawaz/org/sqlitedb20211216/MainActivity.java
+++ app/src/main/java/haqnawaz/org/sqlitedb20211216/MainActivity.java
@@ -1,15 +1,20 @@
 package haqnawaz.org.sqlitedb20211216;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -33,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         buttonViewAll = findViewById(R.id.buttonViewAll);
         btnDelAll = findViewById(R.id.delBtn);
 
-        recyclerView = findViewById(R.id.myRecyclerView);
+        recyclerView = findViewById(R.id.RecyclerView);
         layoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
 
@@ -42,29 +47,68 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                try {
-                    studentModel = new StudentModel(editName.getText().toString(), Integer.parseInt(editAge.getText().toString()), switchIsActive.isChecked());
-                    Toast.makeText(MainActivity.this, studentModel.toString(), Toast.LENGTH_SHORT).show();
-                }
-                catch (Exception e){
-                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                }
-                DbHelper dbHelper = new DbHelper(MainActivity.this);
-                 dbHelper.addStudent(studentModel);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+                builder.setTitle("Add Student");
+                builder.setView(inflater.inflate(R.layout.studentinfo, new LinearLayout(MainActivity.this), false))
+                        .setPositiveButton("Add student", (dialog, id) -> {
+                            editName = ((AlertDialog) dialog).findViewById(R.id.nameEt);
+                            editAge = ((AlertDialog) dialog).findViewById(R.id.ageEt);
+                            switchIsActive = ((AlertDialog) dialog).findViewById(R.id.switchStudent);
+                            if( TextUtils.isEmpty(editName.getText()) || TextUtils.isEmpty(editAge.getText()))
+                            {
+                                Toast.makeText(MainActivity.this, "Fill all the details", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                try
+                                {
+                                    studentModel = new StudentModel(editName.getText().toString(), Integer.parseInt(editAge.getText().toString()), switchIsActive.isChecked());
+                                    DbHelper dbHelper = new DbHelper(MainActivity.this);
+                                    dbHelper.addStudent(studentModel);
+                                    Toast.makeText(MainActivity.this, "Student Added Successfully", Toast.LENGTH_SHORT).show();
+                                    Handler handler = new Handler();
+                                    handler.postDelayed(() -> ViewStudents(), 1000);
+
+                                }
+                                catch (Exception e){
+                                    Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+                                }}
+                        })
+                        .setNegativeButton("Cancel", (dialog, id) -> dialog.cancel());
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
             }
         });
 
-        buttonViewAll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        buttonViewAll.setOnClickListener(v -> ViewStudents());
+
+        btnDelAll.setOnClickListener(view -> {
+            if(layoutManager.getItemCount()>0)
+            {
                 DbHelper dbHelper = new DbHelper(MainActivity.this);
-                List<StudentModel> list = dbHelper.getAllStudents();
-                ArrayAdapter arrayAdapter = new ArrayAdapter<StudentModel>(MainActivity.this, android.R.layout.simple_list_item_1,list);
-                listViewStudent.setAdapter(arrayAdapter);
+                list = dbHelper.getAllStudents();
 
-
-
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("All Students will be permanent deleted");
+                builder.setTitle("Delete Students");
+                builder.setCancelable(false);
+                builder.setPositiveButton("Delete", (dialog, asdf) -> {
+                    RecyclerViewAdapter adapter = new RecyclerViewAdapter(list);
+                    recyclerView.setAdapter(adapter);
+                    dbHelper.deleteAll();
+                    list.clear();
+                    Toast.makeText(MainActivity.this, "All Students Deleted Successfully", Toast.LENGTH_SHORT).show();
+                });
+                builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                dbHelper.close();
             }
+            else
+            {
+                Toast.makeText(MainActivity.this, "There are not any students", Toast.LENGTH_SHORT).show();
+            }
+
         });
 
     }
